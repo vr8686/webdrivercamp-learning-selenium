f90db4faa9b7e74abc1230aab28d6f9eb574268c
vr8686 <145728663+vr8686@users.noreply.github.com>
2023-11-05 22:43:36 -0500

    Finished Selenium exercise

--- selenium_basics/search.py
+++ selenium_basics/search.py
@@ -1,24 +1,26 @@
-nums = [3, 4, 9, 6, 4]
-target = 12
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
 
-# for i in nums:
-#     for x in nums[nums.index(i)+1:]:
-#         if i + x == target:
-#             result = [nums.index(i), nums.index(x)]
-#             print(result)
+driver.get("https://ebay.com")
+print('Waiting for the page to load...')
+search_bar = wait.until(ec.element_to_be_clickable((By.XPATH, '//input[@id="gh-ac"]')))
+print(f"The page has been loaded. Current url is: {driver.current_url}")
 
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+search_input = "women watch"
+print(f"Searching for \"{search_input}\"...")
+search_bar.send_keys('women watch')
+search_bar.send_keys(Keys.RETURN)
 
-        seen = {}
-
-        for i, num in enumerate(nums):
-            if target - num in seen:
-                return [seen[target - num], i]
-            elif num not in seen:
-                seen[num] = i
-
-
-sol = Solution()
-print(sol.twoSum(nums, target))
+header = wait.until(ec.visibility_of_element_located(
+    (By.XPATH, "//h1[contains(@class, 'count-heading')]")))
+header_text = header.text
+print(f"Found in header: {header_text}")
+assert f"results for {search_input}" in header_text
+print('Verified. The header contains “results for women watch“')
+driver.quit()
