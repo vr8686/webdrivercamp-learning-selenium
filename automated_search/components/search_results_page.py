6747e012e732335237f4736c90e14757038c0f4c
vr8686 <145728663+vr8686@users.noreply.github.com>
2023-11-13 22:36:15 -0500

    Finished Automated Search exercise

---
+++ automated_search/components/search_results_page.py
@@ -0,0 +1,43 @@
+from automated_search.components.base import Base
+from selenium.webdriver.common.by import By
+
+
+class SearchResults(Base):
+    def __init__(self, driver, wait):
+        super().__init__(driver, wait)
+
+    @classmethod
+    def real_item_number(cls, item: int):
+        real_number = 0
+        if 1 <= item <= 8:
+            real_number = item + 1
+        elif 9 <= item <= 60:
+            real_number = item + 2
+        elif -52 <= item <= -1:
+            real_number = 63 - abs(item)
+        elif -60 <= item <= -53:
+            real_number = 62 - abs(item)
+        return real_number
+
+    def open_item_in_new_tab(self, item_number: int):
+        real_item_number = SearchResults.real_item_number(item_number)
+        item_locator = (By.XPATH, f'//li[@data-gr4="{real_item_number}"]//span[@role="heading"]')
+        print(f'Opening item #{item_number} in the new tab...')
+        self.click_element(item_locator)
+        print(f"Opening page... Current url is: {self.driver.current_url}")
+        self.switch_webdriver_to_the_new_tab()
+
+    def get_title_text(self, item_number) -> str:
+        real_item_number = SearchResults.real_item_number(item_number)
+        item_title_locator = f'//li[@data-gr4="{real_item_number}"]//span[@role="heading"]'
+        title_text = self.get_text(item_title_locator)
+        #print(f'Item #{item_number} title is: {title_text}')
+        return title_text
+
+    def get_price(self, item_number) -> float:
+        real_item_number = SearchResults.real_item_number(item_number)
+        item_price_locator = f'//li[@data-gr4="{real_item_number}"]//span[@class="s-item__price"]'
+        text = self.get_text(item_price_locator)
+        price = self.extract_float_price(text)
+        print(f'Item #{item_number} price is: {price}')
+        return price
---
+++ automated_search/search.py
@@ -0,0 +1,70 @@
+from automated_search.components.base import Base
+from automated_search.components import verification
+from automated_search.components.item_page import ItemPage
+from automated_search.components.left_filter_panel import LeftFilterPanel
+from automated_search.components.search_results_page import SearchResults
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
+
+base = Base(driver, wait)  # Initializing Base class
+left_filter_panel = LeftFilterPanel(base.driver, base.wait)  # Initializing LeftFilterPanel class
+item_page = ItemPage(base.driver, base.wait)  # Initializing ItemPage class
+search_results = SearchResults(base.driver, base.wait)  # Initializing Search Results Page class
+
+link = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1313&_nkw=watch&_sacat=0"
+
+# Step1. Open eBay watch page
+base.open_url(link)  # Opening the link
+
+# Step2. Select option Brand / Rolex in Filter panel
+brand = "rolex"  # Defining the brand to select
+left_filter_panel.select_brand(brand)  # Selecting the brand on filter panel
+
+# Step3. Verify the first two result items contain “rolex” in their title
+items_to_check = [1, 2]  # numbers as you SEE them on the page
+for item in items_to_check:
+    verification.verify_title_contains(search_results.get_title_text(item), brand)
+
+# Step4. Store title and price of the first two results in a variable
+search_results_data = {"item1 - title": search_results.get_title_text(items_to_check[0]),
+                       "item1 - price": search_results.get_price(items_to_check[0]),
+                       "item2 - title": search_results.get_title_text(items_to_check[1]),
+                       "item2 - price": search_results.get_price(items_to_check[1])
+                       }
+
+# Step5. Open item in and verify the title and the price by comparing them with the stored data
+# Opening item #1 and storing the data
+search_results.open_item_in_new_tab(items_to_check[0])
+item_pages_data = {"item1 - title": item_page.get_title_text(),
+                   "item1 - price": item_page.get_price()
+                   }
+base.close_tab()
+# Opening item #2 and storing the data
+search_results.open_item_in_new_tab(items_to_check[1])
+item_pages_data.update({"item2 - title": item_page.get_title_text(),
+                        "item2 - price": item_page.get_price()
+                        })
+base.close_tab()
+# Comparing the title and the price with stored data
+verification.compare_data(search_results_data, item_pages_data)
+
+# Step6. Uncheck “Rolex“ option
+left_filter_panel.deselect_brand(brand)
+
+# Step7. Check “Casio“ option
+brand = "casio"  # Defining the brand to select
+left_filter_panel.select_brand(brand)  # Selecting the brand on filter panel
+
+# Step8. Verify the last two result items contain “Casio“ in their title
+items_to_check = [-1, -2]
+for item in items_to_check:
+    verification.verify_title_contains(search_results.get_title_text(item), brand)
+
+# Step9. Save and print all the mismatches if any
+verification.verification_report()
+
+# Closing the browser
+base.close_browser()
